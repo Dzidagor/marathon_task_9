6019c108e9ec9244026e44b106a93d6ff5f150c3
Dzidagor <Dzidagorz@mail.ru>
2022-12-02 21:56:08 +0300

    done

---
+++ js/script.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+     genres: [],
+    privat: false
+    },
+    counter = 0;
+
+while (counter < 2) {
+  const movie = prompt("Один из последних просмотренных фильмов?", ""),
+      rate = prompt("На сколько оцените его?", "");
+  if (movie.length == 0 || rate.length == 0 || movie == null || rate == null || movie.length > 50) {
+    continue;
+  }
+  counter++;
+  personalMovieDB.movies[movie] = rate;
+}
+
+if (numberOfFilms < 10){
+  alert("Просмотрено довольно мало фильмов");
+} else if (numberOfFilms < 31) {
+  alert("Вы классический зритель");
+} else if (numberOfFilms > 30){
+  alert("Вы киноман");
+} else {
+  alert("Произошла ошибка")
+}
+
+console.log(personalMovieDB)
